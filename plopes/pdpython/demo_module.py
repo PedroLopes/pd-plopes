a36b906328308170e8f28e5534fc91593fa6399c
Pedro Lopes <plopesresearch@gmail.com>
2015-12-29 15:01:40 +0100

    added a reloader to automatically reload python modules for realtime coding in pure data using pdpython

---
+++ plopes/pdpython/demo_module.py
@@ -0,0 +1,13 @@
+# demo_module.py : trivial Python module to demonstrate reloading pure Python code into a running Pd
+
+class TestClass:
+
+    def __init__(self):
+        pass
+
+    def get_value(self):
+
+        return 'You can change me in realtime and then just hit reload on pure data side'
+    
+
+    
