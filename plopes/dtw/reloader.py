19b839628646268d1264f3c9b32a4673fbd309e7
Pedro Lopes <plopesresearch@gmail.com>
2016-01-05 14:50:10 +0100

    there was a bug with reloader, with debug flag, commented for now

--- plopes/dtw/reloader.py
+++ plopes/dtw/reloader.py
@@ -1,11 +1,16 @@
 import dtw_distance
 import pdgui
 class Reloader:
-    DEBUG = False
+    #DEBUG = False
     def __init__(self):
-        if (self.DEBUG) pdgui.post("Started reloader for:" + dtw_distance)
+        #if self.DEBUG pdgui.post("Started reloader for:" + dtw_distance)
+        pdgui.post("Started reloader for:" + dtw_distance)
         return
     def reload_demo_module(self):
         reload(dtw_distance)
-        if (self.DEBUG) pdgui.post("Done reloading:" + dtw_distance)
+        #if self.DEBUG: pdgui.post("Done reloading:" + dtw_distance)
+        pdgui.post("Done reloading:" + dtw_distance)
         return
+    #def set_debug(self,flag):
+    #    self.DEBUG = flag
+    #    return
